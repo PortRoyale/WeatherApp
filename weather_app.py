d1832daff4f42ee111befe6c0e206f2fc40c9157
Tyler Casper <casperw@colorado.edu>
2020-07-01 19:28:42 -0500

    Basic Weather Application is working.

--- weather_app.py
+++ weather_app.py
@@ -1,13 +1,13 @@
 # 7/1/2020   Tyler Casper
 # I am attempting to build a weather app for my desktop. I am also attempting to use Git version control during this process.
 
-import requests, json
+import requests
+import json
+from tkinter import *
 
+
+# API 
 url = "https://community-open-weather-map.p.rapidapi.com/weather"
-
-city = "Springfield, Illinois"
-
-querystring = {"units":"imperial","q":{city}}
 
 
 # my RapidAPI credentials
@@ -16,7 +16,32 @@ headers = {
     'x-rapidapi-key': "f51bCvoKijmshiTNqux0PbeIpalLp1NynqDjsngk4K3TgypbZ3"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
 
-d = json.loads(response.text)
-print(d)
+# TKinter
+window = Tk()
+
+window.title("Weather Application")
+window.geometry('400x250')
+
+lbl = Label(window, text="Enter City, State:")
+lbl.grid(column=0, row=4)
+
+# Retrieve the city, state data upon click
+def clicked():
+    city = txt.get()
+    querystring = {"units":"imperial","q":{city}}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    dictionary = json.loads(response.text)
+    print(dictionary)
+
+btn = Button(window, text="Retrieve Weather", command = clicked)
+btn.grid(column=2, row=4)
+
+txt = Entry(window,width=30)
+txt.grid(column=1, row=4)
+txt.focus()
+
+window.mainloop()
+
+
+
